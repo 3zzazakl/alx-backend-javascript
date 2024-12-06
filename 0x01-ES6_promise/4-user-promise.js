7d02f02e286a911a1725cb4421b9d6a4be8db6be
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 01:21:49 +0200

    checking first, last name

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+function singUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName: firstName,
+    lastName: lastName,
+  });
+}
+
+export default singUpUser;
