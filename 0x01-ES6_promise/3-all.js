a3dc91a0148cb5074976f07461738cabd2640b50
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 01:00:52 +0200

    checking uploaded photo

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photoResult, userResult] = results;
+      console.log(
+        `${photoResult.body} ${userResult.firstName} ${userResult.lastName}`
+      );
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
