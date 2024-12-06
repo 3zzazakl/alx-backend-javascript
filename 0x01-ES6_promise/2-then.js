da229c26567a85e9fcc3aa2e44a9de28c9b58e29
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 00:40:44 +0200

    checking rejected code and succcess

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+
+function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
+
+export default handleResponseFromAPI;
