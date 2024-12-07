1d21c5d24b6137cdad643b5ac5f402e09ea6d61d
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 03:32:41 +0200

    checking divide

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return numerator / denominator;
+}
+
+export default divideFunction;
