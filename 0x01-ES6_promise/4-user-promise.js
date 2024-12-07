2c2d1de0011a8a1cc6c75efd17e134680f738294
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 03:11:53 +0200

    correct name of function

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
-function singUpUser(firstName, lastName) {
+function signUpUser(firstName, lastName) {
   return Promise.resolve({
     firstName,
     lastName,
   });
 }
 
-export default singUpUser;
+export default signUpUser;
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-import sigUpUser from './4-user-promise';
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
-    sigUpUser(firstName, lastName),
+    signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((results) => results.map((result) => ({
     status: result.status,
