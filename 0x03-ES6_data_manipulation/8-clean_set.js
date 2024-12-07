79de0395e8c72e415d17cd0ee74b28754b241112
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 19:55:56 +0200

    checking task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 function cleanSet(set, startString) {
+  if ([...set].some((value) => typeof value !== 'string')) {
+    return '';
+  }
+
   const result = [...set]
     .filter((value) => typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length))
