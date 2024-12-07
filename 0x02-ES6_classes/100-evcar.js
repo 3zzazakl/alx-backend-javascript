561e4ccb0d1c2d4a4448f02a13a7bad7c1f6026b
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 08:42:46 +0200

    checking task19

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -6,13 +6,8 @@ class EVCar extends Car {
     this._range = range;
   }
 
-  static get [Symbol.species]() {
-    return this;
-  }
-
   cloneCar() {
-    const Species = this.constructor[Symbol.species];
-    return new Species();
+    return new Car(this._brand, this._motor, this._color);
   }
 }
 
