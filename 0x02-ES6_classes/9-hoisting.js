54cef5a2dde0aafa7e23c6caf2e0dea94bd9f57d
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 08:06:54 +0200

    check errors

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -12,9 +12,6 @@ export class ALXClass {
     return this._location;
   }
 }
-
-const class2019 = new ALXClass(2019, 'San Francisco');
-const class2020 = new ALXClass(2020, 'San Francisco');
 export class StudentALX {
   constructor(firstName, lastName, alxClass) {
     this._firstName = firstName;
@@ -34,6 +31,8 @@ export class StudentALX {
     return `${self._firstName} ${self._lastName} - ${self._alxClass.year} - ${self._alxClass.location}`;
   }
 }
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
 
 const student1 = new StudentALX('Guillaume', 'Salva', class2020);
 const student2 = new StudentALX('John', 'Doe', class2020);
@@ -41,4 +40,6 @@ const student3 = new StudentALX('Albert', 'Clinton', class2019);
 const student4 = new StudentALX('Donald', 'Bush', class2019);
 const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
