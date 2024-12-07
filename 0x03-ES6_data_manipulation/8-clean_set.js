2f13d9a3a9d77966bcc9445d45284f4657b83050
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 19:47:02 +0200

    checking task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 function cleanSet(set, startString) {
   const result = [...set]
-    .filter((value) => value.startsWith(startString))
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
   return result;
