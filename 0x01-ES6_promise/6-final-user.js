e78b096dba19d1b54ba3e7b3ac91d493bd1b4721
3zzazakl <vanzekovichgr@gmail.com>
2024-12-07 02:59:30 +0200

    checking task6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+
+import sigUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    sigUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
+}
+
+export default handleProfileSignup;
