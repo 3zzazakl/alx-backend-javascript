77d91ba1201f8db80a504b433094e8ff1501df2b
3zzazakl <vanzekovichgr@gmail.com>
2025-01-23 21:29:08 +0200

    setting last name of previous certificate

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -12,9 +12,6 @@ export class HolbertonClass {
     return this._location;
   }
 }
-
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
 export class StudentHolberton {
   constructor(firstName, lastName, alxClass) {
     this._firstName = firstName;
@@ -34,6 +31,9 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName} - ${this._alxClass.year} - ${this._alxClass.location}`;
   }
 }
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
