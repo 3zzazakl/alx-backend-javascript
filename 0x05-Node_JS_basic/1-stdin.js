ad7ec667b07b37c923d1b43552fb4e78ca0816e8
3zzazakl <vanzekovichgr@gmail.com>
2025-01-26 08:12:46 +0200

    ch

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 // using stdin
-console.log("Welcome to ALX, what is your name?");
+console.log('Welcome to ALX, what is your name?');
 
 process.stdin.on('readable', () => {
-    const name = process.stdin.read();
-    if (name !== null) {
-        process.stdout.write(`Your name is: ${name}`);
-    }
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
